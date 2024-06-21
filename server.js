635ee3e1267a3115f1e0759474527ac3a51f7ee2
Vladimir1000 <vladimir.dzelovski@gmail.com>
2024-06-21 13:19:47 -0500

    update json file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const db = require('./db')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('This is our root page!')
+})
+
+module.exports = app
