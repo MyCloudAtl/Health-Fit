5c06c7ccfcc9b1d19377c23edfce393072a68974
sebcuellar6 <seabasscuellarh@gmail.com>
2024-06-21 13:51:11 -0500

    fixed

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(cors())
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.listen(PORT, () => {
-  console.log(`Express server listening on port ${PORT}`)
-})
+    console.log(`Express server listening on port ${PORT}`)
+  })
 
 app.get('/', (req, res) => {
   res.send('This is our root page!')
@@ -22,6 +22,17 @@ app.get('/', (req, res) => {
 
 app.get('/gyms', gymController.getGyms)
 app.get('/gyms/:id', gymController.getGym)
-
+  
 module.exports = app
 
+app.get('/nutrition', nutritionController.getAllNutrition)
+
+app.get('/nutrition/:id', nutritionController.getNutritionById)
+
+app.post('/nutrition', nutritionController.createNutrition)
+
+app.put('/nutrition/:id', nutritionController.updateNutrition)
+
+app.delete('/nutrition/:id', nutritionController.deleteNutrition)
+
+app.get('*', (req,res) => res.send('404 page not found'))
